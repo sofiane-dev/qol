2394eb76eba0bcbcd50c04230f399b5dbb3201cf
Sofiane Benmahiddine <sofiane.benmahiddine@spintechs.net>
2021-03-27 13:22:35 +0100

    first commit

---
+++ src/main/java/com/qol/qolproject/QolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qol.qolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QolProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qol/qolproject/QolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qol.qolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
